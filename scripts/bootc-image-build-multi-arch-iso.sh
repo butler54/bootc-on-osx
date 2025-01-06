763a70d0be5d48708defd49471ba868fc62973b1
Chris Butler <chris.butler@redhat.com>
2025-01-06 21:32:56 +1100

    fix: add stuff
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

--- scripts/bootc-image-build-multi-arch-iso.sh
+++ scripts/bootc-image-build-multi-arch-iso.sh
@@ -1,5 +1,5 @@
-sudo rm -rf ./output
-mkdir ./output
+sudo rm -rf ./iso
+mkdir ./iso
 sudo podman run \
     --rm \
     -it \
@@ -7,10 +7,11 @@ sudo podman run \
     --pull=newer \
     --security-opt label=type:unconfined_t \
     -v $(pwd)/config.toml:/config.toml:ro \
-    -v $(pwd)/output:/output \
+    -v $(pwd)/iso:/output \
     -v /var/lib/containers/storage:/var/lib/containers/storage \
     quay.io/centos-bootc/bootc-image-builder:latest \
     --type qcow2 \
     --rootfs xfs \
     --config /config.toml \
+    --help
   quay.io/rh-ee-chbutler/fedora:prod
