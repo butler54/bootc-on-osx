920c49169f886a48f0489c1062311b08ee250fe4
Chris Butler <chris.butler@redhat.com>
2025-01-06 12:59:42 +1100

    fix: add extra scripts
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

---
+++ scripts/bootc-image-build-multi-arch.sh
@@ -0,0 +1,17 @@
+sudo rm -rf ./output
+mkdir ./output
+sudo podman run \
+    --rm \
+    -it \
+    --privileged \
+    --pull=newer \
+    --security-opt label=type:unconfined_t \
+    -v $(pwd)/config.toml:/config.toml:ro \
+    -v $(pwd)/output:/output \
+    -v /var/lib/containers/storage:/var/lib/containers/storage \
+    quay.io/centos-bootc/bootc-image-builder:latest \
+    --type qcow2 \
+    --local \
+    --rootfs xfs \
+    --config /config.toml \
+  quay.io/rh-ee-chbutler/fedora:prod
